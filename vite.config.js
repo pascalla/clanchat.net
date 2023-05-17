36a20ece1d197e9a8606f27fa02a2f2a4707893d
Joshua Blackman <joshua.blackman@Joshuas-Mac-Mini.local>
2023-05-17 21:31:18 +0100

    update vite resource locations

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
