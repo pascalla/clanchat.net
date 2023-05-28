5129e37794bd27ba009e4368754c259b0e19e471
Joshua Blackman <joshuadennisblackman@gmail.com>
2023-05-28 12:14:37 +0100

    add horizon for job monitoring

--- app/Jobs/ProcessMessage.php
+++ app/Jobs/ProcessMessage.php
@@ -2,13 +2,10 @@
 
 namespace App\Jobs;
 
-use App\Models\Clan;
-use App\Models\ClanMessage;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
